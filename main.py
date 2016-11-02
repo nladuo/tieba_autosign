0d197272a6851ed3a66b6a206e42e1cc469a6f58
nladuo <kalen25115@gmail.com>
2016-11-02 18:54:42 +0800

    updated with verify_code.

--- main.py
+++ main.py
@@ -7,43 +7,17 @@ import time
 from bs4 import BeautifulSoup
 from config import *
 
-if __name__ == '__main__':
-    print("正在模拟登陆百度贴吧....")
-    # 模拟登陆
-    driver = webdriver.PhantomJS(executable_path=exe_path, desired_capabilities=dcap)
-    driver.get('http://tieba.baidu.com/?page=user')
 
-    # 隐藏跳转app界面
-    app_continue_span = driver.find_element_by_id('index-app-continue')
-    app_continue_span.click()
-    time.sleep(1)
+# 判断是否存在验证码
+def have_verifycode(page_source):
+    bs_obj = BeautifulSoup(page_source, "html.parser")
+    return bs_obj.find('input', {'id': 'TANGRAM__PSP_6__verifycode'}) != None
 
-    # 显示登陆窗口
-    login_tag = driver.find_element_by_class_name('j_footer_toast')
-    login_tag.click()
-    time.sleep(1)
 
-    # 开始模拟登陆
-    username_input = driver.find_element_by_id("TANGRAM__PSP_6__username")
-    username_input.send_keys(username)
-    passwd_input = driver.find_element_by_id("TANGRAM__PSP_6__password")
-    passwd_input.send_keys(passwd)
-
-    login_btn = driver.find_element_by_id("TANGRAM__PSP_6__submit")
-    login_btn.click()
-    time.sleep(1)
-
-    print ('登陆成功, 开始签到!')
-
-    # 开始进行签到
-    driver.get("http://tieba.baidu.com/?page=like")
-    expand_more = driver.find_element_by_xpath("//div[@class='expand-all']/p")
-    expand_more.click()
-    time.sleep(1)
-
-    # 获取常逛的贴吧
+# 解析贴吧
+def parse_tiebas(page_source):
     tiebas = []
-    bs_obj = BeautifulSoup(driver.page_source, "html.parser")
+    bs_obj = BeautifulSoup(page_source, "html.parser")
     for a in bs_obj.find_all('a', {'class': 'j_forumTile_wrapper'}):
         title = a.get("data-start-app-param")
         href = a.get("href")
@@ -51,27 +25,74 @@ if __name__ == '__main__':
             'title': title,
             'href': 'http://tieba.baidu.com' + href
         })
+    return tiebas
 
-    # 逐个签到
-    for tieba in tiebas:
-        print(u"正在签到-->\"" + tieba['title'] + u"\"吧", end = '')
-        driver.get(tieba['href'])
-        bs_obj = BeautifulSoup(driver.page_source, "html.parser")
-        sign_btn_tag = bs_obj.find("a", {'class': 'sign-button'})
-        if sign_btn_tag.text == u"已签到":
-            print("(已经完成签到,不需要重新签到)")
-            continue
-        # 点击签到
-        sign_btn = driver.find_element_by_class_name('sign-button')
-        sign_btn.click()
+if __name__ == '__main__':
+    print("正在模拟登陆百度贴吧....")
+    # 模拟登陆
+    driver = webdriver.PhantomJS(executable_path=exe_path, desired_capabilities=dcap)
+    try:
+        driver.get('http://tieba.baidu.com/?page=user')
+
+        # 隐藏跳转app界面
+        app_continue_span = driver.find_element_by_id('index-app-continue')
+        app_continue_span.click()
         time.sleep(1)
 
-        # 关闭贴吧客户端窗口
-        close_client_btn = driver.find_element_by_class_name('daoliu_sign_in_prompt_close')
-        close_client_btn.click()
+        # 显示登陆窗口
+        login_tag = driver.find_element_by_class_name('j_footer_toast')
+        login_tag.click()
         time.sleep(1)
-        print("(签到成功)")
 
-    print('全部签到完成!!!')
-    driver.quit()
+        # 开始模拟登陆
+        username_input = driver.find_element_by_id("TANGRAM__PSP_6__username")
+        username_input.send_keys(username)
+        passwd_input = driver.find_element_by_id("TANGRAM__PSP_6__password")
+        passwd_input.send_keys(passwd)
+
+        login_btn = driver.find_element_by_id("TANGRAM__PSP_6__submit")
+        login_btn.click()
+        time.sleep(1)
+
+        # 判断验证码
+        if have_verifycode(driver.page_source):
+            print('遇到验证码, 自动退出')
+        else:
+            print('登陆成功, 开始签到!')
+            # 开始进行签到
+            driver.get("http://tieba.baidu.com/?page=like")
+            expand_more = driver.find_element_by_xpath("//div[@class='expand-all']/p")
+            expand_more.click()
+            time.sleep(1)
+
+            # 获取常逛的贴吧
+            tiebas = parse_tiebas(driver.page_source)
+
+            # 逐个签到
+            for tieba in tiebas:
+                print(u"正在签到-->\"" + tieba['title'] + u"\"吧", end='')
+                driver.get(tieba['href'])
+                bs_obj = BeautifulSoup(driver.page_source, "html.parser")
+                sign_btn_tag = bs_obj.find("a", {'class': 'sign-button'})
+                if sign_btn_tag.text == u"已签到":
+                    print("(已经完成签到,不需要重新签到)")
+                    continue
+                # 点击签到
+                sign_btn = driver.find_element_by_class_name('sign-button')
+                sign_btn.click()
+                time.sleep(1)
+
+                # 关闭贴吧客户端窗口
+                close_client_btn = driver.find_element_by_class_name('daoliu_sign_in_prompt_close')
+                close_client_btn.click()
+                time.sleep(1)
+                print("(签到成功)")
+
+            print('全部签到完成!!!')
+        driver.quit()
+    except Exception, ex:
+        print(ex)
+        print('正在退出')
+        driver.quit()
+
 
